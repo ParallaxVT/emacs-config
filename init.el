feb0a5917ad6b869f2999a431b6f4fb7429be233
rafaelgp <mr.rafaelgp@gmail.com>
2012-04-11 16:24:31 +0100

    Change the way the buffer names is displayed if they have similar names

--- init.el
+++ init.el
@@ -139,6 +139,12 @@
 ;; highlight matching parenthesis
 (show-paren-mode t)
 (setq show-paren-style 'parenthesis)
+;; meaningful names for buffers with the same name
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-separator "/")
+(setq uniquify-after-kill-buffer-p t)    ; rename after killing uniquified
+(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
 
 (message "Editor options loaded...")
 
