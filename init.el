b9a4b79c380c76137fa4fe9d709e91a6e792bdc8
rafaelgp <mr.rafaelgp@gmail.com>
2012-04-12 18:17:38 +0100

    Added settings for eshell

--- init.el
+++ init.el
@@ -394,6 +394,29 @@ there's a region, all lines that region covers will be duplicated."
 (setq yas/prompt-functions '(yas/dropdown-prompt))
 
 ;;}}}
+;;{{{ ESHELL
+
+(setq eshell-ask-to-save-history (quote always))
+(setq eshell-history-size 1000)
+(setq eshell-ask-to-save-history 'always)
+(setq eshell-cmpl-cycle-completions nil)
+(setq eshell-ls-dired-initial-args (quote ("-h")))
+(setq eshell-ls-initial-args "-h")
+(setq eshell-ls-use-in-dired t)
+;; scroll to the bottom
+(setq eshell-output-filter-functions (quote (eshell-handle-control-codes eshell-watch-for-password-prompt eshell-postoutput-scroll-to-bottom)))
+(setq eshell-scroll-show-maximum-output t)
+(setq eshell-scroll-to-bottom-on-output t)
+(setq eshell-glob-include-dot-dot nil)
+
+(defun eshell/clear ()
+  "clear the eshell buffer."
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)))
+
+;;}}}
+
 
 (message "Pacakges options loaded...")
 
