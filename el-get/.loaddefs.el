d0b38ededa7535da32f4863c63be37a3b8d6b4d3
rafaelgp <mr.rafaelgp@gmail.com>
2012-04-19 12:36:59 +0100

    Add el-get/.loadderfs.el

---
+++ el-get/.loaddefs.el
@@ -0,0 +1,17 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("profile-dotemacs/profile-dotemacs.el")
+;;;;;;  (20367 55431 773437))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
