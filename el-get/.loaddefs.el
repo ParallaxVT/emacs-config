6782c1b8eed465bfd58827d7fc058beee7caabb6
rafaelgp <mr.rafaelgp@gmail.com>
2012-07-16 16:56:46 +0100

    Deleted el-get

--- el-get/.loaddefs.el
+++
@@ -1,17 +0,0 @@
-;;; .loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("profile-dotemacs/profile-dotemacs.el")
-;;;;;;  (20367 55431 773437))
-
-;;;***
-
-(provide '.loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; .loaddefs.el ends here
