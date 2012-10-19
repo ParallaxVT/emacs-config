f3e61b9e3041da463eb1ea013a53fcb74667971e
rafaelgp <mr.rafaelgp@gmail.com>
2012-10-19 06:54:22 +0100

    Added org-publish function to export all the org filse in org/ folder to public_html

--- init.el
+++ init.el
@@ -507,6 +507,39 @@ If there's a text selection, work on the selected text."
 ;;; remove 'validate XHTML' link at the bottom
 (setq org-export-html-postamble nil)
 
+(require 'org-publish)
+(setq org-publish-project-alist
+      '(
+        ("B-inherit"
+         :base-directory "~/org/"
+         :recursive t
+         :base-extension "css\\|js"
+         :publishing-directory "~/public_html/"
+         :publishing-function org-publish-attachment
+         )
+        ("org-notes"
+         :base-directory "~/org/"
+         :base-extension "org"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-publish-org-to-html
+         :headline-levels 4             ; Just the default for this project.
+         ;; :style "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style.css\" />"
+         :auto-preamble n
+         :auto-sitemap t                ; Generate sitemap.org automagically...
+         :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
+         :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+         :sitemap-style "tree"
+         )
+        ("org-static"
+         :base-directory "~/org/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/public_html/"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+        ("org" :components ("org-notes" "org-static"))
+        ))
 ;;}}}
 ;;{{{ ESHELL
 
