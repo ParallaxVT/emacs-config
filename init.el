ba0c7d017f58f78c5ef6e5ac9b4d5c1a9b680cdc
rafaelgp <mr.rafaelgp@gmail.com>
2012-12-13 13:17:55 +0000

    Added to the path files in the first level within .emacs/ and .emacs/vendor

--- init.el
+++ init.el
@@ -1,1 +1,27 @@
+;; Emacs configuration file
+
+;; determine the load path dirs
+;; as relative to the location of this file
+(defvar dotfiles-dir "~/.emacs/"
+  "The root Emacs Lisp source folder")
+
+;; external packages reside here
+(defvar ext-dir (concat dotfiles-dir "vendor/")
+  "The root folder for external packages")
+
+
+(defun add-subfolders-to-load-path (parent-dir)
+  "Adds all first level `parent-dir' subdirs to the
+Emacs load path."
+  (dolist (f (directory-files parent-dir))
+    (let ((name (concat parent-dir f)))
+      (when (and (file-directory-p name)
+                 (not (equal f ".."))
+                 (not (equal f ".")))
+        (add-to-list 'load-path name)))))
+
+;; add the first level subfolders automatically
+(add-subfolders-to-load-path dotfiles-dir)
+(add-subfolders-to-load-path ext-dir)
+
 (load "~/.emacs/conf/backup_and_autosave")
