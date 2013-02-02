15626517ad3ee9f47bda2983ccfc52f2f58c32f0
rafaelgp <mr.rafaelgp@gmail.com>
2013-02-02 17:08:19 +0000

    After a really bad merge, deleted bunch of files that shouldn't be there

--- el-get/.loaddefs.el
+++
@@ -1,17 +0,0 @@
-;;; .loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("el-get/el-get-install.el" "el-get/el-get.el")
-;;;;;;  (20484 14967 847000 0))
-
-;;;***
-
-(provide '.loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; .loaddefs.el ends here
