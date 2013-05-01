c79c2c01739ed79c2b3d642b2b6b2ee5c5cbb58e
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-01 16:02:20 +0100

    vendor.el -> added file

---
+++ vendor/vendor.el
@@ -0,0 +1,17 @@
+; for loading libraries in from the vendor directory
+(defun vendor (library)
+  (let* ((file (symbol-name library))
+         (normal (concat "~/.emacs/vendor/" file))
+         (suffix (concat normal ".el")))
+    (cond
+     ((file-directory-p normal)
+      (add-to-list 'load-path normal)
+      (require library))
+     ((file-directory-p suffix)
+      (add-to-list 'load-path suffix)
+      (require library))
+     ((file-exists-p suffix)
+      (require library)))))
+
+(vendor 'fill-column-indicator)
+(vendor 'linum-off)
