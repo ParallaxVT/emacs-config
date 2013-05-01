014eac2d9af27057880cc108228a95e0f33ac1c4
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-01 15:56:43 +0100

    conf_smex -> Substitute 'setq' for 'defvar' to avoid warnings when it's compiled

--- conf/conf_smex.el
+++ conf/conf_smex.el
@@ -1,5 +1,9 @@
+;; ====================
+;; conf_smex.el
+;; ====================
 ;; Show frequently used commands that have no key bindings 
 ;; smex-show-unbound-commands
 
 ;; Put smex file in misc folder
-(setq smex-save-file (concat dotfiles-dir "misc/.smex-items")) 
+(defvar smex-save-file (concat dotfiles-dir "misc/.smex-items")) 
+
