3088a9a85f35d1db4906ac1f5ea33fa551c1de58
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-01 15:53:45 +0100

    conf_autosave.el -> Substitute 'setq' for 'defvar' to avoind warnings when it's compiled

--- conf/conf_autosave.el
+++ conf/conf_autosave.el
@@ -1,9 +1,12 @@
+;; ====================
+;; conf_autosave.el
+;; ====================
 (defvar user-temporary-autosave-directory (concat dotfiles-dir "autosave")
   "Directory to store all the autosave files")
 
 (make-directory user-temporary-autosave-directory t) ; Create directory if doesn't exists already
 
-(setq
+(defvar
  auto-save-file-name-transforsm            ; place all auto-save files in a dedicated directory
 `((".*" ,user-temporary-autosave-directory t))
  )
@@ -11,3 +14,4 @@
 (global-auto-revert-mode t)                ; revert buffers every 5 seconds
 
 (provide 'conf_autosave)
+
