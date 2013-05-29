27d0e5dc5f61a10b06eed7ad51bbd769d2459b8d
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-29 17:30:08 +0100

    Add hippie_expand configuration

---
+++ conf/conf_hippie_expand.el
@@ -0,0 +1,17 @@
+;; ====================
+;; Hippie-expand
+;; ====================
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+                                         try-expand-dabbrev-all-buffers
+                                         try-expand-dabbrev-from-kill
+                                         try-complete-file-name-partially
+                                         try-complete-file-name
+                                         try-expand-all-abbrevs
+                                         try-expand-list
+                                         try-expand-line
+                                         try-complete-lisp-symbol-partially
+                                         try-complete-lisp-symbol))
+(provide 'conf_hippie_expand)
