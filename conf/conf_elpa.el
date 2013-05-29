69451b748e439b06f06914071c206c4a345e4acd
rafaelgp <mr.rafaelgp@gmail.com>
2013-05-29 16:22:19 +0100

    Added melpa url. Added function to check a list of packages and install them if they are not installed.

--- conf/conf_elpa.el
+++ conf/conf_elpa.el
@@ -2,3 +2,30 @@
 ;; conf_elpa.el
 ;; ====================
 (defvar package-user-dir (concat dotfiles-dir "elpa"))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;;           '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(defvar packages-list
+  '(smex)
+  "A list of packages to ensure are installed at launch.")
+
+(defun check-packages-installed ()
+  (loop for p in packages-list
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (check-packages-installed)
+  ;; check for new packages (package versions)
+  (message "%s" "Refreshing package database")
+  (package-refresh-contents)
+  (message "%s" " Done!")
+  ;; install the missing packages
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'packages-list)
