1695a41993001dcd4297d0fbfd37d04dcc932e2b
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:45:10 +0100

    mod -> added mmm_mode.el

---
+++ mod/mod_mmm_mode.el
@@ -0,0 +1,13 @@
+;; ====================
+;; mod_mmm_mode.el
+;; ====================
+
+(require 'mmm-auto)
+(setq mmm-global-mode 'maybe)
+(setq mmm-submode-decoration-level 0)
+;; Set up an mmm group for fancy html editing
+(mmm-add-mode-ext-class 'html-mode nil 'html-js)
+(mmm-add-mode-ext-class 'html-mode nil 'html-css)
+(mmm-add-mode-ext-class 'html-mode nil 'html-css-attribute)
+
+(provide 'mod_mmm_mode)
