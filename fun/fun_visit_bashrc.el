516927c06407e13a01686f45153a97ecd2ac23fe
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:03:38 +0100

    fun -> Put all the functions in functions.el file

--- fun/fun_visit_bashrc.el
+++
@@ -1,9 +0,0 @@
-;; =======================
-;; fun_visit_bashrc.el
-;; =======================
-(defun visit-bashrc ()
-  "as in Load Custom"
-  (interactive)
-  (find-file "~/.bashrc")
-  )
-(provide 'fun_visit_bashrc)
--- fun/fun_visit_init.el
+++
@@ -1,9 +0,0 @@
-;; =====================
-;; fun_visit_init.el
-;; =====================
-(defun vinit ()
-  "As in visit-init"
-  (interactive)
-  (find-file "~/.emacs/init.el")
-  )
-(provide 'fun_visit_init)
