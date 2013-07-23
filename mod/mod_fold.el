22a528b0ab93e3e213e0522bf45d24b703bf50ac
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:43:54 +0100

    mod -> added fold.el

---
+++ mod/mod_fold.el
@@ -0,0 +1,11 @@
+;; ====================
+;; mod_fold.el
+;; ====================
+
+(require 'fold)
+(add-hook 'sgml-mode-hook 'fold-mode)
+(add-hook 'nxml-mode-hook 'fold-mode)
+(add-hook 'css-mode-hook 'fold-mode)
+(add-hook 'emacs-lisp-mode-hook 'fold-mode)
+
+(provide 'mod_fold)
