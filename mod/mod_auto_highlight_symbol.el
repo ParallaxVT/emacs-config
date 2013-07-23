840db7d4aa6b0e787aa1c2c7e4bcfbeca27db6cb
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:21:04 +0100

    mod -> added auto-highlight-symbol

---
+++ mod/mod_auto_highlight_symbol.el
@@ -0,0 +1,8 @@
+;; ============================
+;; mod_auto_highlight_symbol.el
+;; ============================
+
+(require 'auto-highlight-symbol)
+(global-auto-highlight-symbol-mode t)
+
+(provide 'mod_auto_highlight_symbol)
