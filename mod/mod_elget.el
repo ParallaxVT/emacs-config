c35bd50080e3cc965b08334e98bd1eb7b68a28c7
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:57:24 +0100

    mod -> added mod_elget.el

--- mod/mod_elget.el
+++ mod/mod_elget.el
@@ -2,4 +2,21 @@
 ;; mod_elget.el
 ;; ====================
 
+(defvar el-get-dir (expand-file-name "el-get/" dotfiles-dir)
+  "Custom directory for el-get")
+
+(add-to-list 'load-path (concat dotfiles-dir "el-get/el-get"))
+
+;; Downlead el-get if it's not installed
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path (concat dotfiles-dir "el-get-user/recipes"))
+
+(el-get 'sync)
+
 (provide 'mod_elget)
