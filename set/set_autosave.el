aec8be32cb33647280d055db73b8ffe768e1705e
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:52:30 +0100

    set -> Renamed conf_autosave.el to set_autosave.el

--- set/set_autosave.el
+++ set/set_autosave.el
@@ -1,6 +1,10 @@
 ;; ====================
-;; conf_autosave.el
+;; set_autosave.el
 ;; ====================
+
+(defvar dotfiles-dir "~/.emacs/"
+  "The root dir of my emacs files.")
+
 (defvar user-temporary-autosave-directory (concat dotfiles-dir "autosave")
   "Directory to store all the autosave files")
 
@@ -13,5 +17,20 @@
 
 (global-auto-revert-mode t)                ; revert buffers every 5 seconds
 
-(provide 'set_autosave)
+;; Remember my location in a file when saving files
+(setq save-place-file (concat dotfiles-dir "autosave/saveplace"))
+;; Savehist keeps track of some history
+(setq savehist-additional-variables
+      ;; Search entries
+      '(search ring regexp-search-ring)
+      ;; Save every minute
+      savehist-autosave-interval 60
+      ;; Keep the home clean
+      savehist-file (concat dotfiles-dir "autosave/savehist"))
+(savehist-mode t)
+;; Save recent files
+(setq recentf-save-file (concat dotfiles-dir "autosave/recentf")
+      recentf-max-saved-items 200
+      recentf-max-menu-items 15)
 
+(provide 'set_autosave)
