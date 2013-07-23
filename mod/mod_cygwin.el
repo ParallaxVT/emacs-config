146bdb0e504113cd2c862f95ac7d349dc8e0f773
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:57:37 +0100

    mod -> added mod_cygwin.el

--- mod/mod_cygwin.el
+++ mod/mod_cygwin.el
@@ -2,4 +2,12 @@
 ;; mod_cygwin.el
 ;; ====================
 
+;; Prevent issues with the Windows null device (NUL)
+;; when using cygwin find with rgrep.
+(defadvice grep-compute-defaults (around grep-compute-defaults-advice-null-device)
+  "Use cygwin's /dev/null as the null-device."
+  (let ((null-device "/dev/null"))
+    ad-do-it))
+(ad-activate 'grep-compute-defaults)
+
 (provide 'mod_cygwin)
