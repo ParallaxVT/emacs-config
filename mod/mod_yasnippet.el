4940c2a01c5b20be61cca84d24a11f8bf5b10f52
rafaelgp <mr.rafaelgp@gmail.com>
2013-07-23 15:56:41 +0100

    mod -> added mod_yasnippet.el

--- mod/mod_yasnippet.el
+++ mod/mod_yasnippet.el
@@ -2,4 +2,18 @@
 ;; mod_yasnippet.el
 ;; ====================
 
+(require 'yasnippet)
+
+(add-hook 'html-mode-hook 'yas/minor-mode-on)
+(add-hook 'css-mode-hook 'yas/minor-mode-on)
+(add-hook 'php-mode-hook 'yas/minor-mode-on)
+(add-hook 'nxml-mode-hook 'yas/minor-mode-on)
+
+(setq yas/root-directory "~/.emacs/snippets/")
+
+(yas/load-directory yas/root-directory)
+
+(require 'dropdown-list)
+(setq yas/prompt-functions '(yas/dropdown-prompt))
+
 (provide 'mod_yasnippet)
