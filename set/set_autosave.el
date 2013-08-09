664b6b808cea2bf28f3048f049f7cd9748783863
rafaelgp <mr.rafaelgp@gmail.com>
2013-08-09 13:54:52 +0100

    set_autosave.el -> Fix indentation

--- set/set_autosave.el
+++ set/set_autosave.el
@@ -2,18 +2,15 @@
 ;; set_autosave.el
 ;; ====================
 
-(defvar dotfiles-dir "~/.emacs/"
-  "The root dir of my emacs files.")
-
 (defvar user-temporary-autosave-directory (concat dotfiles-dir "autosave")
   "Directory to store all the autosave files")
 
 (make-directory user-temporary-autosave-directory t) ; Create directory if doesn't exists already
 
 (defvar
- auto-save-file-name-transforsm            ; place all auto-save files in a dedicated directory
-`((".*" ,user-temporary-autosave-directory t))
- )
+  auto-save-file-name-transforsm            ; place all auto-save files in a dedicated directory
+  `((".*" ,user-temporary-autosave-directory t))
+  )
 
 (global-auto-revert-mode t)                ; revert buffers every 5 seconds
 
