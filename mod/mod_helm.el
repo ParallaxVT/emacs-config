7669e0a352a5ac1c6c7eaa3bdc925a99023f2e62
rafaelgp <mr.rafaelgp@gmail.com>
2013-08-09 13:33:50 +0100

    mod_helm.el -> Increase idle delay to .3

--- mod/mod_helm.el
+++ mod/mod_helm.el
@@ -6,8 +6,8 @@
 
 (helm-mode 1)
 
-(defvar helm-buffer-max-length 40)
-(defvar helm-idle-delay 0.1)
-(defvar helm-input-idle-delay 0.1)
+(setq helm-buffer-max-length 40)
+(setq helm-idle-delay .3)
+(setq helm-input-idle-delay .3)
 
 (provide 'mod_helm)
