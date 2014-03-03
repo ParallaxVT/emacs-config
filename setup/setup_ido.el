70f35af7f5cc75c298c7ea7ae6e8a36e695b3252
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 10:44:54 +0000

    Add ido settings file

---
+++ setup/setup_ido.el
@@ -0,0 +1,22 @@
+;; ====================
+;; setup_ido.el
+;; ====================
+
+(require 'ido)
+
+(setq ido-enable-flex-matching t ;; enable fuzzy search
+      ido-everywhere t
+      ido-create-new-buffer 'always ;; create a new buffer if no buffer matches substring
+      ido-file-extensions-order '(".sv" ".v" ".svh" ".tv" ".m" ".c" ".cpp" ".emacs")
+      ;;  customize the order in which files are sorted when Ido displays them in
+      ;; the minibuffer. There are certain file extensions I use more than others,
+      ;; so I tell Ido to emphasize those
+      ido-use-filename-at-point 'guess ;; find file at point using ido
+      ido-auto-merge-work-directories-length 0 ;; look into other directories if
+      ;; the entered filename doesn't exist in current directory
+      ;; ido-auto-merge-work-directories-length -1 ;; do NOT look into other directories if
+      ;;                  ;; the entered filename doesn't exist in current directory
+      )
+(ido-mode 1)
+
+(provide 'setup_ido)
