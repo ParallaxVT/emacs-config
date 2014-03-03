dddecb8e641a32f6a6021938dc27115d0d7049db
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 10:40:09 +0000

    Change appearance settings

--- setup/setup_appearance.el
+++ setup/setup_appearance.el
@@ -34,11 +34,13 @@
 ;; Gui
 ;; ====================
 
-(tool-bar-mode -1) ;; Hide tool bar
-(menu-bar-mode 1)  ;; Show menu bar
+(tool-bar-mode -1)                                 ;; Hide tool bar
+(menu-bar-mode 1)                                  ;; Show menu bar
 (blink-cursor-mode -1)                             ;; Cursor, stop blinking!
 (setq inhibit-startup-screen (user-login-name))    ;; Start up screen? No thanks
-(setq initial-scratch-message ";; Scratch Buffer") ;; New scratch buffer text
+(setq initial-scratch-message
+      (concat ";; Initialization successful, welcome to "
+              (substring (emacs-version) 0 16) ".")) ;; New scratch buffer text
 
 ;; ====================
 ;; Editor behavior
@@ -55,7 +57,9 @@
 (show-paren-mode t)                             ;; Highlight matching parents
 (setq show-paren-style 'parenthesis)            ;; Set parenthesis as a parent
 (global-hl-line-mode +1)                        ;; Highlight the current line
-(setq next-line-add-newlines t)                 ;; Don't let next-line add new lines at end of file
+(setq next-line-add-newlines t)                 ;; Make sure text files end in a new line
+(setq inhibit-default-init t)                   ;; Disable loading of defoult.el at startup
+
 ;; ====================
 ;; Buffer
 ;; ====================
@@ -72,9 +76,10 @@
 
 (setq scroll-margin 0
       scroll-conservatively 100000
-      scroll-preserve-screen-position 1)  ;; Smooth scrolling
-(windmove-default-keybindings)            ;; Switch between buffers using shift + arrows
-(setq truncate-partial-width-windows nil) ;; Truncate lines that are too long
+      scroll-preserve-screen-position 1)                     ;; Smooth scrolling
+(windmove-default-keybindings)                               ;; Switch between buffers using shift + arrows
+(setq truncate-partial-width-windows nil)                    ;; Truncate lines that are too long
+(setq frame-title-format '(buffer-file-name "%f" ("%b")))    ;;  Set the title bar to show file name if available, buffer name otherwise
 
 ;; ====================
 ;; Mode line
@@ -89,15 +94,17 @@
 ;; ====================
 
 (if (fboundp 'fringe-mode)
-    (fringe-mode 1))  ;; Make the sides fringe thinner
+    (fringe-mode 5))  ;; Make the sides fringe thinner
 (global-linum-mode t) ;; show line numbers please!
 
 ;; ====================
 ;; Indent
 ;; ====================
 
-(setq-default indent-tabs-mode nil)  ;; Don't use tabs to indent
-(setq-default tab-width 8)           ;; Width of a TAB character
+(setq tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
+(setq indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)  ;; Always indent using spaces never tabs
 
 ;; ====================
 ;; Search
@@ -122,4 +129,4 @@
   (recenter))
 (ad-activate 'isearch-repeat-backward)
 
-(provide 'set_appearance)
+(provide 'setup_appearance)
