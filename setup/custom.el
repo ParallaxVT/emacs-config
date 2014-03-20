221a66b619dbb6f969befb2c694aebd4f704734c
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-20 20:14:46 +0000

    Updated the path to the bookmarks directory

--- setup/custom.el
+++ setup/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "c:\\Users\\rafaelgp\\AppData\\Roaming\\.emacs.d\\bookmarks"))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
