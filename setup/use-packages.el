4b12867176787d45575eae34ace069307acfb5e8
RafaelGP <mr.rafaelgp@gmail.com>
2014-11-19 13:43:32 +0000

    Make xml-mode config a hook

--- setup/use-packages.el
+++ setup/use-packages.el
@@ -28,6 +28,11 @@
              (setq sgml-basic-offset 4)
              (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
              (setq indent-tabs-mode nil)))
+(add-hook 'xml-mode-hook
+          '(lambda()
+             (setq tab-width 4)
+             (setq c-basic-offset 4)
+             (setq indent-tabs-mode nil)))
 
 ;; Keybindings
 (bind-key "C-c n"                             'cleanup-buffer)
@@ -537,16 +542,6 @@
     (add-hook 'web-mode-hook #'(lambda () (yas-activate-extra-mode 'html-mode)
                                  (yas-activate-extra-mode 'css-mode)
                                  (evil-leader/set-key "t" 'web-mode-comment-or-uncomment)))))
-
-(use-package xml-mode
-  :disabled t
-  :init
-  (progn
-    (add-hook 'xml-mode-hook
-              '(lambda()
-                 (setq tab-width 4)
-                 (setq c-basic-offset 4)
-                 (setq indent-tabs-mode nil)))))
 
 (use-package yasnippet
   :disabled t
