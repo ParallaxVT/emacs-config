4aaec19ec4cf957c2fb0fa6b49018445555b18e3
RafaelGP <mr.rafaelgp@gmail.com>
2015-02-09 15:02:17 +0000

    Deleted code to delete first 4 lines in rgrep output. I gave up using grep in windows. Now I use The Silver Searcher (Ag)

--- setup/setup_defun.el
+++ setup/setup_defun.el
@@ -245,20 +245,6 @@ Position the cursor at its beginning, according to the current mode."
   (recenter))
 (ad-activate 'isearch-repeat-backward)
 
-;; Rgrep
-;; ==================================================================
-
-;; Delete the first 4 lines in the rgrep outut buffer
-;; Type (C-x n w) to reveal this lines
-(defun delete-grep-header ()
-  (save-excursion
-    (with-current-buffer grep-last-buffer
-      (goto-line 5)
-      (narrow-to-region (point) (point-max)))))
-
-(defadvice grep (after delete-grep-header activate) (delete-grep-header))
-(defadvice rgrep (after delete-grep-header activate) (delete-grep-header))
-
 ;; Text Editing
 ;; ==================================================================
 
