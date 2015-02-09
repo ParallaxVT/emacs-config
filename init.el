fb5f3a53a610d60dbeab931f3bcfd96719809856
RafaelGP <mr.rafaelgp@gmail.com>
2015-02-09 14:59:55 +0000

    Updated paths to find,xargs,grep and added cmdproxy.

--- init.el
+++ init.el
@@ -301,9 +301,10 @@
 
 ;; Make rgrep find the executables it needs in windows
 (when (or (eq system-type 'windows-nt) (eq system-type 'msdos))
-  (setq find-program "C:\\msysgit\\bin\\find"
-        xargs-program "C:\\msysgit\\bin\\xargs"
-        grep-program "C:\\msysgit\\bin\\grep"))
+  (setq find-program "C:\\msysgit\\bin\\find.exe"
+        xargs-program "C:\\msysgit\\bin\\xargs.exe"
+        grep-program "C:\\msysgit\\bin\\grep.exe"
+        shell-file-name "C:\\msysgit\\bin\\cmdproxy.exe"))
 
 (defadvice shell-quote-argument (after windows-nt-special-quote (argument) activate)
   "Add special quotes to ARGUMENT in case the system type is 'windows-nt."
