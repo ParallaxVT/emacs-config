bdda30bbdb80320f7cc6c8398399ffc25d7bdb30
RafaelGP <mr.rafaelgp@gmail.com>
2015-04-09 19:35:34 +0100

    Added ':defer 4' to yasnippet

--- setup/setup_packages.el
+++ setup/setup_packages.el
@@ -651,6 +651,7 @@
   :ensure t
   :if (not noninteractive)
   :diminish yas-minor-mode
+  :defer 4
   :commands (yas-minor-mode yas-expand)
   :mode ("/\\.emacs\\.d/snippets/" . snippet-mode)
   :init
